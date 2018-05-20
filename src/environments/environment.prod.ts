76abb83adee4ae360032f02adc2b297cab8e6d9e
charnpreetsingh <csingh02@calpoly.edu>
2018-05-20 16:10:00 -0700

    firebase prod values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC_GTVDJsPGuikfyLT_3pbjXedN4pwFo14",
+    authDomain: "polyrides-1524115747538.firebaseapp.com",
+    databaseURL: "https://polyrides-1524115747538.firebaseio.com",
+    projectId: "polyrides-1524115747538",
+    storageBucket: "",
+    messagingSenderId: "293174586025"
+  }
 };
