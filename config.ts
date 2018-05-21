eeafcb6456e6a5e6f2ec866c28a35e0782af8676
charnpreetsingh <csingh02@calpoly.edu>
2018-05-20 18:01:47 -0700

    check for .env file

--- config.ts
+++ config.ts
@@ -1,15 +1,19 @@
+const path = require('path');
 const dotenv = require('dotenv');
 const replace = require('replace-in-file');
 
 // Read OS environment variable
-const result = dotenv.config();
+// Netlify will already have this set but local dev requires .env file
+if (path.existsSync('.env')) {
+  const result = dotenv.config();
 
-if (result.error) {
-  throw result.error;
+  if (result.error) {
+    throw result.error;
+  }
+
+  console.log(result.parsed);
+  console.log("Environment parsed!");
 }
-
-console.log(result.parsed);
-console.log("Environment parsed!");
 
 const gmapsKey = process.env.gmaps;
 const options = {
