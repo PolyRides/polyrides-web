04eec366db15bc3f9abddbfb51d67827cfc71159
charnpreetsingh <csingh02@calpoly.edu>
2018-05-20 18:04:19 -0700

    path replaced with fs

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
-const path = require('path');
+const fs = require('fs');
 const dotenv = require('dotenv');
 const replace = require('replace-in-file');
 
 // Read OS environment variable
 // Netlify will already have this set but local dev requires .env file
-if (path.existsSync('.env')) {
+if (fs.existsSync('.env')) {
   const result = dotenv.config();
 
   if (result.error) {
