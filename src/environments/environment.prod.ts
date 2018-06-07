80b217a4ab61d1e754b7b75fc26bd76330b8c117
charnpreetsingh <csingh02@calpoly.edu>
2018-06-06 20:19:24 -0700

    testing PROD firebase instance

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,19 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyC_GTVDJsPGuikfyLT_3pbjXedN4pwFo14",
-    authDomain: "polyrides-1524115747538.firebaseapp.com",
-    databaseURL: "https://polyrides-1524115747538.firebaseio.com",
-    projectId: "polyrides-1524115747538",
-    storageBucket: "",
-    messagingSenderId: "293174586025"
+    apiKey: "AIzaSyB3FWioF57Nmlx2F_-eRV_Y6qlP0AAU64E",
+    authDomain: "polyrides-3d79d.firebaseapp.com",
+    databaseURL: "https://polyrides-3d79d.firebaseio.com",
+    projectId: "polyrides-3d79d",
+    storageBucket: "polyrides-3d79d.appspot.com",
+    messagingSenderId: "792863860294"
   },
   gmapsKey: '{GMAPS_KEY}'
 };
+
+//   apiKey: "AIzaSyC_GTVDJsPGuikfyLT_3pbjXedN4pwFo14",
+//   authDomain: "polyrides-1524115747538.firebaseapp.com",
+//   databaseURL: "https://polyrides-1524115747538.firebaseio.com",
+//   projectId: "polyrides-1524115747538",
+//   storageBucket: "",
+//   messagingSenderId: "293174586025"
